e8b568c50e98d0bfd95ced35b5e04d84f407fe6e
Jesse Harlan <jesse.harlan@centralia.edu>
2024-09-24 13:54:47 -0700

    Add project files.

---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,9 @@
+﻿using ObserverPattern;
+
+Console.WriteLine("Hello, Design Patterns!");
+
+School school = new School(1, "Centralia");
+
+school.ToggleOpenStatus();
+
+Console.WriteLine("The School is " + school.IsSchoolOpen);
---
+++ ObserverPattern/School.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    public class School
+    {
+        // properties of a school
+        public int SchoolId { get; set; }
+        public string SchoolName { get; set; }
+        public bool IsSchoolOpen { get; set; }
+
+
+        // methods
+        public School(int num, string name)
+        {
+            SchoolId = num;
+            SchoolName = name;
+            IsSchoolOpen = true;
+        }
+
+        public void ToggleOpenStatus()
+        {
+            IsSchoolOpen = !IsSchoolOpen;
+        }
+
+        public void CloseSchool()
+        {
+            // do stuff here
+            IsSchoolOpen = false;
+        }
+    }
+}
